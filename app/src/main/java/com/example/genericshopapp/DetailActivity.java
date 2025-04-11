7f1e31577ff3d880b2c4416e504b4ac93ce5284d
graphXDoses <xridoses@gmail.com>
2025-04-11 23:43:21 +0300

    Added todos. A simple mock transitioning between activities (so far).

--- app/src/main/java/com/example/genericshopapp/DetailActivity.java
+++ app/src/main/java/com/example/genericshopapp/DetailActivity.java
@@ -2,7 +2,9 @@ package com.example.genericshopapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // TODO: Implement Backend logic for activity. This showcase
+        //       is temporary.
+        findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
