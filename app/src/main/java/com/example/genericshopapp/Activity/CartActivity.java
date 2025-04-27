8b3720308f72612ac97e1e7b1eb21dc9d7e6f936
Markella Ioannidou <markella.ion12@gmail.com>
2025-04-28 02:20:00 +0300

    Implementation of price calculator and UI initialization

--- app/src/main/java/com/example/genericshopapp/Activity/CartActivity.java
+++ app/src/main/java/com/example/genericshopapp/Activity/CartActivity.java
@@ -6,19 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.genericshopapp.Helper.ManagmentCart;
 import com.example.genericshopapp.R;
+import com.example.genericshopapp.databinding.ActivityCartBinding;
 
 public class CartActivity extends AppCompatActivity {
 
+    ActivityCartBinding binding;
+    private double tax;
+    private ManagmentCart managementCart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityCartBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_cart);
 
-        // TODO: Implement Backend logic for activity. This showcase
-        //       is temporary.
+        managementCart = new ManagmentCart(this);
+        
+        calculatorCart();
+        setVariable();
+        initCartList();
         
         findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +36,43 @@ public class CartActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void setVariable() {
+        binding.backBtn.setOnClickListener(view -> {
+
+        });
+    }
+
+    private void initCartList() {
+
+        if(managementCart.getListCart().isEmpty()){
+
+            binding.emptyTxt.setVisibility(View.GONE);
+            binding.scrollView2.setVisibility(View.GONE);
+
+        }else{
+
+            binding.emptyTxt.setVisibility(View.GONE);
+            binding.scrollView2.setVisibility(View.VISIBLE);
+
+        }
+        
+    }
+
+    private void calculatorCart() {
+
+        double percentTax = 0.02;
+        double delivery = 10;
+        tax = Math.round((managementCart.getTotalFee() * percentTax*100.0))/100.0;
+
+        double total = Math.round(managementCart.getTotalFee() + tax + delivery *100.0)/ 100.0;
+        double itemTotal = Math.round((managementCart.getTotalFee()*100.0))/100.0;
+
+        binding.subtotalTxt.setText("$" + itemTotal);
+        binding.taxTxt.setText("$" + tax);
+        binding.deliveryTxt.setText("$" + delivery);
+        binding.totalTxt.setText("$" + total);
     }
 
 }
