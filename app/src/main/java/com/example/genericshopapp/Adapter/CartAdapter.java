2d9e7335673f9d283275435d1a6f1c779e3c9c35
Markella Ioannidou <markella.ion12@gmail.com>
2025-04-29 14:33:57 +0300

    Cart Adapter Implementation

---
+++ app/src/main/java/com/example/genericshopapp/Adapter/CartAdapter.java
@@ -0,0 +1,71 @@
+package com.example.genericshopapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.genericshopapp.Domain.ItemsModel;
+import com.example.genericshopapp.Helper.ChangeNumberItemsListener;
+import com.example.genericshopapp.Helper.ManagmentCart;
+import com.example.genericshopapp.databinding.ViewholderCartBinding;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.Viewholder>{
+
+    ArrayList<ItemsModel> listItemSelected;
+    ChangeNumberItemsListener changeNumberItemsListener;
+    private ManagmentCart managementCart;
+
+    public CartAdapter(ArrayList<ItemsModel> listItemSelected, Context context, ChangeNumberItemsListener changeNumberItemsListener) {
+        this.listItemSelected = listItemSelected;
+        this.changeNumberItemsListener = changeNumberItemsListener;
+        managementCart = new ManagmentCart(context);
+    }
+
+    @NonNull
+    @Override
+    public CartAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ViewholderCartBinding binding = ViewholderCartBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+
+        return new Viewholder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.Viewholder holder, int position) {
+        holder.binding.titleTxt.setText(listItemSelected.get(position).getTitle());
+        holder.binding.feeEachItem.setText("$" + listItemSelected.get(position).getPrice());
+        holder.binding.totalEachItem.setText("$" +Math.round((listItemSelected.get(position).getNumberInCart() *
+                listItemSelected.get(position).getPrice())));
+        holder.binding.numberItemTxt.setText(listItemSelected.get(position).getNumberInCart());
+
+        Glide.with(holder.itemView.getContext()).load(listItemSelected.get(position).getPicUrl().get(0)).into(holder.binding.pic);
+
+        holder.binding.plusCartBtn.setOnClickListener(view -> managementCart.plusItem(listItemSelected, position, () -> {
+            notifyDataSetChanged();
+            changeNumberItemsListener.changed();
+        }));
+
+        holder.binding.minusCartBtn.setOnClickListener(view -> managementCart.minusItem(listItemSelected, position, () -> {
+            notifyDataSetChanged();
+            changeNumberItemsListener.changed();
+        }));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItemSelected.size();
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        ViewholderCartBinding binding;
+        public Viewholder(ViewholderCartBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
